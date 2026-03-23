273774d81c79253dbed15a027abd63df050d8cb9
Sergio <sergito91@gmail.com>
2026-03-23 17:08:50 +0100

    Corregido client feign.

--- msvc-cursos/src/main/java/com/sergio/microservicios/cursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/com/sergio/microservicios/cursos/clients/UsuarioClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-usuarios", url = "${clients.usuarios.url}")
+@FeignClient(name = "msvc-usuarios", url = "${CLIENTS_USUARIOS_URL:${clients.usuarios.url}}")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
--- msvc-usuarios/src/main/java/com/sergio/msvc/usuarios/clients/CursoClientRest.java
+++ msvc-usuarios/src/main/java/com/sergio/msvc/usuarios/clients/CursoClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-cursos", url="${clients.cursos.url}")
+@FeignClient(name="msvc-cursos", url="${CLIENTS_CURSOS_URL:${clients.cursos.url}}")
 public interface CursoClientRest {
 
     @DeleteMapping("/eliminar-usuario/{id}")
