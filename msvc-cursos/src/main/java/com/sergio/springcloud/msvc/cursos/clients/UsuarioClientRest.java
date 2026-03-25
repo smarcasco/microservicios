04a9362d21f67e0450952f763eb54fd68e43532f
Sergio <sergito91@gmail.com>
2026-03-25 09:41:25 +0100

    Corregidos erreos en tests.

---
+++ msvc-cursos/src/main/java/com/sergio/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -0,0 +1,20 @@
+package com.sergio.springcloud.msvc.cursos.clients;
+
+import com.sergio.springcloud.msvc.cursos.models.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name="msvc-usuarios", url="${msvc.usuarios.url}")
+public interface UsuarioClientRest {
+
+    @GetMapping("/{id}")
+    Usuario detalle(@PathVariable Long id);
+
+    @PostMapping("/")
+    Usuario crear(@RequestBody Usuario usuario);
+
+    @GetMapping("/usuarios-por-curso")
+    List<Usuario> obtenerAlumnosPorCurso(@RequestParam Iterable<Long> ids);
+}
