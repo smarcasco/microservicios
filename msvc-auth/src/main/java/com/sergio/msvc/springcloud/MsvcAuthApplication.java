2690cc33ba1d6cc54b2ff5607b58fc079dc0a22f
Sergio <sergito91@gmail.com>
2026-04-01 17:57:10 +0200

    Agregadas aplicaciones.

---
+++ msvc-auth/src/main/java/com/sergio/msvc/springcloud/MsvcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.msvc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAuthApplication.class, args);
+	}
+
+}
