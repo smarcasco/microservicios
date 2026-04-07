bf57708b4d17a7ea00fdd71f34ecddb6f63e5731
Sergio <sergito91@gmail.com>
2026-04-07 12:32:06 +0200

    Agregado oauth en usuarios.

--- msvc-usuarios/src/main/java/com/sergio/msvc/usuarios/security/SecurityConfig.java
+++ msvc-usuarios/src/main/java/com/sergio/msvc/usuarios/security/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +34,25 @@ public class SecurityConfig {
 
         return http.build();
     }
-}
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+            User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build(),
+            User.builder()
+                .username("pepito")
+                .password(passwordEncoder().encode("pepito"))
+                .roles("USER")
+                .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
