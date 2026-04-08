3fb7ef72180b806d1977b4332340d5fc5a5765cd
Sergio <sergito91@gmail.com>
2026-04-08 15:14:51 +0200

    Corregido import.

--- msvc-auth/src/main/java/com/sergio/msvc/springcloud/SecurityConfig.java
+++ msvc-auth/src/main/java/com/sergio/msvc/springcloud/SecurityConfig.java
@@ -30,7 +30,7 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.*;
 import org.springframework.security.web.SecurityFilterChain;
